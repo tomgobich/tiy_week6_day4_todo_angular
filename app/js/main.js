4b6281081cbd9df8921d3d356b370ed98ba08ee7
Tom Gobich <fallofmars@gmail.com>
2016-11-03 19:53:58 -0400

    basic functionality working - not including tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,27 @@
 (() => {
-	
+
 	'use strict';
 
 	angular.module('app', [])
 
 })();
+
+$(document).ready(function() {
+
+	$(document).scroll(function() {
+
+		let yAxis 		= $(document).scrollTop(),
+        	$nav 		= $('#navHeader'),
+			$content	= $('#content');
+
+		if(yAxis >= 75)  {
+	        $nav.css({position: "fixed", "top" : "0", "left" : "0"});
+			$content.css({marginTop: "60px"});
+	    } else {
+	        $nav.css("position", "relative");
+			$content.css({marginTop: "0px"});
+	    }
+
+	});
+
+});
--- dist/js/main.js
+++ dist/js/main.js
@@ -6,3 +6,21 @@
 
 	angular.module('app', []);
 })();
+
+$(document).ready(function () {
+
+	$(document).scroll(function () {
+
+		var yAxis = $(document).scrollTop(),
+		    $nav = $('#navHeader'),
+		    $content = $('#content');
+
+		if (yAxis >= 75) {
+			$nav.css({ position: "fixed", "top": "0", "left": "0" });
+			$content.css({ marginTop: "60px" });
+		} else {
+			$nav.css("position", "relative");
+			$content.css({ marginTop: "0px" });
+		}
+	});
+});

33f0073249ea0ff95ce090287f0840435dc0c7c7
Tom Gobich <fallofmars@gmail.com>
2016-11-04 14:00:35 -0400

    applications base functionality is complete

--- js/factories/FLocalStorage.js
+++ js/factories/FLocalStorage.js
@@ -6,14 +6,25 @@
 
 	angular.module('app').factory('FLocalStorage', function () {
 
+		// ------------------------------------------------------------
+		// Name: set
+		// Abstract: Handling function to save to localStorage
+		// ------------------------------------------------------------
 		var set = function set(name, data) {
-			if (data === '' || data === undefined || data === null) {
-				console.log('ERROR: The data you were trying to save in localStorage is not complete');
+			// Is the data bad?
+			if (data === undefined || data === null) {
+				// Yes, notify of a warning
+				$.notify('WARNING: The data you were trying to save in localStorage is not complete, and your data was not saved. If this problem persists, please contact support');
 			} else {
+				// No, save it to localStorage
 				localStorage.setItem(name, JSON.stringify(data));
 			}
 		};
 
+		// ------------------------------------------------------------
+		// Name: get
+		// Abstract: Handling function to get from localStorage
+		// ------------------------------------------------------------
 		var get = function get(name) {
 			return JSON.parse(localStorage.getItem(name));
 		};
